e441584e2630c919be0691a2fc62abf4ccbbf277
Taehee9 <dhstoalfh9509@naver.com>
2020-01-26 16:45:25 +0900

    posts api controller test

---
+++ src/test/java/com/th/springboot/web/PostsApiControllerTest.java
@@ -0,0 +1,127 @@
+package com.th.springboot.web;
+
+import com.th.springboot.domain.posts.Posts;
+import com.th.springboot.domain.posts.PostsRepository;
+import com.th.springboot.web.dto.PostsSaveRequestDto;
+import com.th.springboot.web.dto.PostsUpdateRequestDto;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * post api controller test
+ * SpringBootTest와 TestRestTemplate을 사용해 JPA 기능까지 한 번에 테스트
+ * RANDOM_PORT로 인한 랜덤포트 실행
+ *
+ * Created by Taehee Kwon,
+ * User : Taehee
+ * Date : 2020-01-25
+ * Time : 오후 7:42
+ * Contact : kwonth9509@gmail.com
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PostsApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    /**
+     * 데이터 다 지우기
+     * @throws Exception exception
+     */
+    @After
+    public void tearDown() throws Exception {
+        postsRepository.deleteAll();
+    }
+
+    /**
+     * 게시글 등록 test
+     * @throws Exception exception
+     */
+    @Test
+    public void posts_register() throws Exception {
+        // given
+        String title = "title";
+        String content = "content";
+
+        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author("author")
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
+        // when
+        // postForEntity = POST 요청을 보내고 결과로 ResponseEntity 반환
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+
+    }
+
+    /**
+     * 게시글 수정 test
+     * @throws Exception exception
+     */
+    @Test
+    public void Posts_modified() throws Exception {
+        // given
+        Posts savedPosts = postsRepository.save(Posts.builder()
+            .title("title")
+            .content("cotent")
+            .author("author")
+            .build());
+
+        Long updateId = savedPosts.getId();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate
+                .exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
+    }
+}
